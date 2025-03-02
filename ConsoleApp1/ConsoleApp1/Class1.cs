ceb609bb810c01fd9ff2b9c237376a198499179d
Adith <adithravindranofficial@gmail.com>
2025-03-02 12:40:14 +0530

    Largest element in array

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public static int Largest(int[] array)
+        {
+            int max = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+
+            }
+            return max;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+var ans = Class1.Largest([1, 34, 43, 56, 3, 5, 300]);
+Console.WriteLine(ans);
