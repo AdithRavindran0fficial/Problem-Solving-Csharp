bc620da68d2e6c4dc721602a4602856188c22238
Adith <adithravindranofficial@gmail.com>
2025-03-02 13:40:17 +0530

    second largest number

---
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class2
+    {
+        public static int SecondLargest(int[] array)
+        {
+            int f = 0;
+            int s = -1;
+            for(int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > f)
+                {
+                    s = f;
+                    f = array[i];
+                }
+                else if (array[i]<f && array[i] > s)
+                {
+                    s = array[i];
+                }
+            }
+            return s;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,5 +2,7 @@
 
 using ConsoleApp1;
 
-var ans = Class1.Largest([1, 34, 43, 56, 3, 5, 300]);
+//var ans = Class1.Largest([1, 34, 43, 56, 3, 5, 300]);
+//Console.WriteLine(ans);
+var ans = Class2.SecondLargest([22, 34,12323, 1, 345]);
 Console.WriteLine(ans);
