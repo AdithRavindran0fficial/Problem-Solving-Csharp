712204494c3eb63b15cf03d19cf9bb2d90bb3524
Adith <adithravindranofficial@gmail.com>
2025-04-13 14:07:10 +0530

    Linear Search

---
+++ Linear Search/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Linear_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var result = class1.FindOccurance([1, 2, 3, 4, 5], 3);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Linear Search/class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search
+{
+    class class1
+    {
+        public static int FindOccurance(int[]nums, int x)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
