caa74a5538d3d569660fd4228fa97005c988593a
Adith <adithravindranofficial@gmail.com>
2025-04-13 14:07:26 +0530

    Find Union

---
+++ Union/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Union
+{
+    class Class1
+    {
+        public static int[] FindUnion(int[] nums, int[] num2, int m, int n)
+        {
+            var list = new List<int>();
+            for(int i = 0; i < m; i++)
+            {
+                list.Add(nums[i]);
+            }
+            for(int i = 0; i < n; i++)
+            {
+                if (!list.Contains(num2[i]))
+                {
+                    list.Add(num2[i]);
+                }
+            }
+            list.Sort();
+            return list.ToArray();
+        }
+    }
+}
---
+++ Union/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Union
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var result = Class1.FindUnion([1, 2, 3, 4, 5], [2, 3, 4, 4, 5], 5, 5);
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
