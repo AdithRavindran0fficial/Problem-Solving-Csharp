58f92ceb0cee8fd937c5dd8bc01ee70c05290cce
Adith <adithravindranofficial@gmail.com>
2026-06-04 20:50:31 +0530

    Check array is sorted rotated

---
+++ check if array is sorted rotated/ConsoleApp1/CheckArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CheckArray
+    {
+        public static bool Check(int[] arr)
+        {
+            int count = 0;  
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[(i + 1) % arr.Length])
+                {
+                    count++;
+                }
+                if(count > 1)
+                {
+                    return false;
+                }       
+            }
+            return true;        
+
+        }
+    }
+}
---
+++ check if array is sorted rotated/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+int[] arr = { 2, 1, 3, 4 };
+bool result = CheckArray.Check(arr);
+Console.WriteLine(result);  
