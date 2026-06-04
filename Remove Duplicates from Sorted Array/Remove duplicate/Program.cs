c0c94841bade3709ca2568501f237422467657c0
Adith <adithravindranofficial@gmail.com>
2026-06-04 21:33:22 +0530

    Remove duplicate from sorted array

---
+++ Remove Duplicates from Sorted Array/Remove duplicate/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Remove_duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int ans = RemoveDuplicate.RemoveDuplicates(new int[] { 1, 1, 2 });
+            Console.WriteLine(ans);
+        }
+           
+    }
+}
---
+++ Remove Duplicates from Sorted Array/Remove duplicate/RemoveDuplicate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_duplicate
+{
+    public class RemoveDuplicate
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
